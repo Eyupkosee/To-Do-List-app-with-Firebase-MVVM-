9fd9d163e048ab131531425ff816fceda0ade9b7
Eyupkosee <89103093+Eyupkosee@users.noreply.github.com>
2023-06-13 15:30:52 +0300

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by eyüp köse on 13.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
